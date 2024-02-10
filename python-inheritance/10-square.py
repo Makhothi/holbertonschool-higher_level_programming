3f6f0869234a94950fe166c08c4ac373cfdeaaa8
Makhothi <147424358+Makhothi@users.noreply.github.com>
2024-02-10 12:06:17 +0200

    Create 10-square.py

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class square """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class of a square"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return super().area()
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)

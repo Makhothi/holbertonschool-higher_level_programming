1285cd13b48d98a771eb77a939ca1840c5a6b5fd
Makhothi <147424358+Makhothi@users.noreply.github.com>
2024-02-10 14:28:24 +0200

    Update 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Definition of Lookup function"""
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """Function return the list of attribute and methods"""
-    return dir(obj)
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
